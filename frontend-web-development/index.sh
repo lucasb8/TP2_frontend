2068d1f813f29b0762f3b00eeac0d998ac9b3e40
Thomas VEILLARD <thomaveillard@live.fr>
2021-02-09 11:25:22 +0100

    Backend publish typing definitions

--- frontend-web-development/index.sh
+++ frontend-web-development/index.sh
@@ -1,3 +1,4 @@
+cp -r build/typings/* dist-typings/
 sleep 5 # waits until the database as started
 echo "CREATE DATABASE $MYSQL_DATABASE" | mysql -h "$MYSQL_HOST" --protocol=tcp -u "$MYSQL_USERNAME" && npm run db:prepare:prod && npm run db:seeds
 echo "CREATE DATABASE ${MYSQL_DATABASE}_test" | mysql -h "$MYSQL_HOST" --protocol=tcp -u "$MYSQL_USERNAME" && npm run db:prepare:test
