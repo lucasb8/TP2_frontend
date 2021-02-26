e6eaff28a7a683f48bc3566af04acc86982c1f33
TruffeCendree <thomasveillard@live.fr>
2021-02-26 17:12:14 +0100

    Update index.sh

--- frontend-web-development/index.sh
+++ frontend-web-development/index.sh
@@ -3,4 +3,3 @@ cp -r build/typings/* dist-typings/
 echo "CREATE DATABASE $MYSQL_DATABASE" | mysql -h "$MYSQL_HOST" --protocol=tcp -u "$MYSQL_USERNAME" && npm run db:prepare:prod && npm run db:seeds
 echo "CREATE DATABASE ${MYSQL_DATABASE}_test" | mysql -h "$MYSQL_HOST" --protocol=tcp -u "$MYSQL_USERNAME" && npm run db:prepare:test
 node --async-stack-traces index.js
-
