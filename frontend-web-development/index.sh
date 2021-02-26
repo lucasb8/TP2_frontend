cb7fc70626fb9ac5d7b442c01f18e6d8aa93cfd5
Thomas VEILLARD <thomaveillard@live.fr>
2021-02-26 12:40:58 +0100

    Fix container entry point for some windows users

--- frontend-web-development/index.sh
+++ frontend-web-development/index.sh
@@ -1,5 +1,6 @@
-cp -r build/typings/* dist-typings/
 sleep 5 # waits until the database as started
+cp -r build/typings/* dist-typings/
 echo "CREATE DATABASE $MYSQL_DATABASE" | mysql -h "$MYSQL_HOST" --protocol=tcp -u "$MYSQL_USERNAME" && npm run db:prepare:prod && npm run db:seeds
 echo "CREATE DATABASE ${MYSQL_DATABASE}_test" | mysql -h "$MYSQL_HOST" --protocol=tcp -u "$MYSQL_USERNAME" && npm run db:prepare:test
-node --async-stack-traces index
+node --async-stack-traces index.js
+
