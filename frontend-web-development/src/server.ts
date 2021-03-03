5bc00d4748d14005de47d6994dbfff153318c1a1
Thomas VEILLARD <thomaveillard@live.fr>
2021-03-03 06:41:50 +0100

    Fix angular destroying dist folder unsync directory with docker

--- frontend-web-development/src/server.ts
+++ frontend-web-development/src/server.ts
@@ -4,13 +4,16 @@ import * as express from 'express'
 import * as session from 'express-session'
 import * as SessionStorage from 'express-mysql-session'
 import * as morgan from 'morgan'
+import * as fs from 'fs'
 import MySQLStore from './mysql-store'
 import config from './config'
 import authRoutes from './routes/auth/routes'
 import apiRoutes from './routes/api/routes'
 import PrintableError from './printable-error'
+import Utils from './utils'
 
 export default () => {
+  const assetsPath = autoDetectAssetsPath()
   const cookieParser = session({
     secret: config.cookies.encryptionKey,
     resave: false,
@@ -37,13 +40,13 @@ export default () => {
   router.use('/docs', express.static('build/docs'))
   router.use('/auth', authRoutes)
   router.use('/api', apiRoutes)
-  router.use(express.static('build/frontend'))
+  router.use(express.static(assetsPath))
 
   // serve static files (static files contains .)
-  router.get('*.*', express.static('build/frontend', {maxAge: '1y'}));
+  router.get('*.*', express.static(assetsPath, { maxAge: '1y' }));
 
   // serve angular bundle when loading an angular route (routes unknown by server)
-  router.get('*', (req, res) => res.status(200).sendFile('/', {root: 'build/frontend'}))
+  router.get('*', (req, res) => res.status(200).sendFile('/', { root: assetsPath }))
 
   router.use(logFailedRequest)
 
@@ -75,4 +78,18 @@ async function overrideResponseJsonSerializer (req: express.Request, res: expres
 async function logFailedRequest (err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
   if (err instanceof PrintableError) res.status(400).json({ messages: err.messages })
   else { console.log(err); next(err) }
+}
+
+function autoDetectAssetsPath () {
+  try {
+    const results = fs.readdirSync('build/frontend/dist').filter(path => path !== '.' && path !== '..')
+
+    if (results.length === 1) return `build/frontend/dist/${results[0]}`
+    else if (results.length === 0) Utils.debug(module, 'Failed to find assets folder. Use build/frontend/dist/frontend instead')
+    else Utils.debug(module, 'Ambiguous assets folder. Use build/frontend/dist/frontend instead')
+  } catch (err) {
+    Utils.debug(module, err.message)
+  }
+
+  return 'build/frontend/dist/frontend'
 }
